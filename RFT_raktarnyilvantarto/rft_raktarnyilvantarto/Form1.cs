cd1f2a04a006869d7af67147d61b3ca5c2916e15
Danisovszky Márk <dmarkprog@gmail.com>
2017-11-06 14:06:14 +0100

    Az eredeti projektben a Megrendelo.cs és a Termek.cs osztály beillesztése és kiegészítése

--- RFT_raktarnyilvantarto/rft_raktarnyilvantarto/Form1.cs
+++ RFT_raktarnyilvantarto/rft_raktarnyilvantarto/Form1.cs
@@ -16,5 +16,10 @@ namespace rft_raktarnyilvantarto
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
