1f017504618a736e7e3e3de14f1d9c7defa9c420
Danisovszky Márk <dmarkprog@gmail.com>
2017-11-06 14:39:12 +0100

    Adatbázis összekapcsolva a programmal Adatbázis mezőinek frissítése

--- RFT_raktarnyilvantarto/rft_raktarnyilvantarto/Form1.cs
+++ RFT_raktarnyilvantarto/rft_raktarnyilvantarto/Form1.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace rft_raktarnyilvantarto
 {
     public partial class Form1 : Form
     {
+        static OleDbConnection Connect = new OleDbConnection();
+
+        static List<Termek> Termekek = new List<Termek>();
+        static List<Megrendelo> Megrendelok = new List<Megrendelo>();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +25,60 @@ namespace rft_raktarnyilvantarto
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void AdatbazisBeolvasasa(string path)
+        {
+            Connect.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + @";User Id=admin;Password=";
+            Connect.Open();
+
+            //Termékek
+            OleDbCommand Command = new OleDbCommand();
+            Command.Connection = Connect;
+            Command.CommandText = "SELECT Vonalkod, Megnevezes, Tipus, Raktar_db, Minimum_db, Megrendelo FROM Termek";
+
+            OleDbDataReader Reader = Command.ExecuteReader();
+            Termek atm = new Termek();
+
+            while (Reader.Read())
+            {
+                atm.Vonalkod = Reader[0].ToString();
+                atm.Nev = Reader[1].ToString();
+                atm.Tipus = Reader[2].ToString();
+                atm.Raktar_db = Convert.ToInt32(Reader[3].ToString());
+                atm.Min_db = Convert.ToInt32(Reader[4].ToString());
+                atm.Megrendelok.Add(Convert.ToInt32(Reader[5].ToString()));
+
+                Termekek.Add(atm);
+
+                atm = new Termek();
+            }
+            Reader.Close();
+            Connect.Close();
+
+            //Megrendelők
+            Connect.Open();
+            Command = new OleDbCommand();
+            Command.Connection = Connect;
+            Command.CommandText = "SELECT Id, Ceg_nev, Weblap, Telefon FROM Megrendelo";
+
+            Reader = Command.ExecuteReader();
+            Megrendelo atm_ = new Megrendelo();
+
+            while (Reader.Read())
+            {
+                atm_.Id = Convert.ToInt32(Reader[0].ToString());
+                atm_.Nev = Reader[1].ToString();
+                atm_.Weblap = Reader[2].ToString();
+                atm_.Telefon = Reader[3].ToString();
+
+                Megrendelok.Add(atm_);
+
+                atm_ = new Megrendelo();
+            }
+            Reader.Close();
+            Connect.Close();
 
         }
     }
